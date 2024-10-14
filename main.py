10f618873f27c3e368e0e03091c35dc9cdd478f4
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-10-14 20:48:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import requests
 
 
 def main():
+    params = {
+        'm': '',
+        'n': '',
+        'q': '',
+        'T': '',
+        'lang=ru': ''
+    }
 
-    url_template= "https://wttr.in/{}?m&n&q&T&lang=ru"
+    url_template= "https://wttr.in/{}"
     locations = ["Лондон", "Шереметьево", "Череповец"]
     
     for location in locations:
         url = url_template.format(location)
 
-        response = requests.get(url)
+        response = requests.get(url, params=params)
         response.raise_for_status()
 
         print(response.text)
